0d4f1c977e37505cca6cebf8076f5a7c254d58dd
howtocenteradiv <mprashant365@gmail.com>
2024-06-17 00:06:48 +0530

    feature section update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
-    console.log(e)
+  console.log(e)
 })
 
 function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
+  lenis.raf(time)
+  requestAnimationFrame(raf)
 }
 
 requestAnimationFrame(raf)
@@ -15,94 +15,94 @@ var textWrapper = document.querySelector('.line-01');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline()
-    .add({
-        targets: '.line-01 .letter',
-        translateY: [200, 0],
-        translateZ: 0,
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 1200,
-        delay: (el, i) => 2400 + 50 * i
-    });
+  .add({
+    targets: '.line-01 .letter',
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 2400 + 50 * i
+  });
 
 var textWrapper = document.querySelector('.line-02');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline()
-    .add({
-        targets: '.line-02 .letter',
-        translateY: [200, 0],
-        translateZ: 0,
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 1200,
-        delay: (el, i) => 2400 + 50 * i
-    });
+  .add({
+    targets: '.line-02 .letter',
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 2400 + 50 * i
+  });
 
 var textWrapper = document.querySelector('.line-03');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline()
-    .add({
-        targets: '.line-03 .letter',
-        translateY: [200, 0],
-        translateZ: 0,
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 1200,
-        delay: (el, i) => 2400 + 50 * i
-    });
+  .add({
+    targets: '.line-03 .letter',
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 2400 + 50 * i
+  });
 
 TweenMax.to(".wrapper", 2, {
-    top: "-100%",
-    ease: Expo.easeInOut,
-    delay: 1.6
+  top: "-100%",
+  ease: Expo.easeInOut,
+  delay: 1.6
 });
 
 var tl = new TimelineMax();
 
 tl.from(".loader", 1, {
-    scaleY: "0%",
-    y: 80,
-    ease: Expo.easeInOut,
-    delay: 1,
-    transformOrigin: "50% 100%"
+  scaleY: "0%",
+  y: 80,
+  ease: Expo.easeInOut,
+  delay: 1,
+  transformOrigin: "50% 100%"
 });
 
 tl.to(".loader", 1, {
-    height: "40vh",
-    scaleY: "0%",
-    ease: Expo.easeInOut,
-    transformOrigin: "0% -100%"
+  height: "40vh",
+  scaleY: "0%",
+  ease: Expo.easeInOut,
+  transformOrigin: "0% -100%"
 });
 
 TweenMax.to(".box", 2.4, {
-    y: "-100%",
-    ease: Expo.easeInOut,
-    delay: 1.6,
+  y: "-100%",
+  ease: Expo.easeInOut,
+  delay: 1.6,
 });
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
 
 
-  const items = document.querySelectorAll(".accordion button");
+const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
-  
+
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute('aria-expanded', 'false');
   }
-  
+
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
   }
@@ -119,8 +119,8 @@ let tabIndicator = tabs.querySelector(".tab-indicator");
 let tabHeaderNodes = tabs.querySelectorAll(".tab-header > div");
 let tabBodyNodes = tabs.querySelectorAll(".tab-body > div");
 
-for(let i=0;i<tabHeaderNodes.length;i++){
-  tabHeaderNodes[i].addEventListener("click",function(){
+for (let i = 0; i < tabHeaderNodes.length; i++) {
+  tabHeaderNodes[i].addEventListener("click", function () {
     tabHeader.querySelector(".active").classList.remove("active");
     tabHeaderNodes[i].classList.add("active");
     tabBody.querySelector(".active").classList.remove("active");
