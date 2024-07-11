f5bc8539dd3859689b99fa416dc71d1187040f95
howtocenteradiv <mprashant365@gmail.com>
2024-07-11 23:52:43 +0530

    updates on couple sections... too long to mention rn.. aaaah (yawns) feeling so lazy man

--- script.js
+++ script.js
@@ -100,21 +100,110 @@ function toggleAccordion() {
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
-// Tabs
 
-let tabs = document.querySelector(".tabs");
-let tabHeader = tabs.querySelector(".tab-header");
-let tabBody = tabs.querySelector(".tab-body");
-let tabIndicator = tabs.querySelector(".tab-indicator");
-let tabHeaderNodes = tabs.querySelectorAll(".tab-header > div");
-let tabBodyNodes = tabs.querySelectorAll(".tab-body > div");
+// Features
 
-for (let i = 0; i < tabHeaderNodes.length; i++) {
-  tabHeaderNodes[i].addEventListener("click", function () {
-    tabHeader.querySelector(".active").classList.remove("active");
-    tabHeaderNodes[i].classList.add("active");
-    tabBody.querySelector(".active").classList.remove("active");
-    tabBodyNodes[i].classList.add("active");
-    tabIndicator.style.left = `calc(calc(calc(25% - 5px) * ${i}) + 10px)`;
-  });
+const slide = {
+  elements: gsap.utils.toArray('.slide'),
+  images: gsap.utils.toArray('.slide-img'),
+  prev: document.querySelector('.slider-button-previous'),
+  next: document.querySelector('.slider-button-next'),
+
+  duration: 1.4,
+  ease: Power4.easeInOut,
 }
+
+let current = 0;
+let total = slide.elements.length;
+const slidesWrap = gsap.utils.wrap(0, total);
+
+
+
+const slideNext = (index, direction = 1, duration = slide.duration) => {
+  const tl = gsap.timeline({ defaults: { duration: duration, ease: slide.ease } });
+
+  tl.fromTo(
+      slide.elements[index],
+      {
+          yPercent: direction > 0 ? 100 : -100,
+      },
+      {
+          yPercent: 0,
+      }
+  ).
+      fromTo(
+          slide.images[index],
+          {
+              yPercent: direction > 0 ? -100 : 100,
+              scale: 0.8,
+          },
+          {
+              yPercent: 0,
+              scale: 1,
+          },
+          0
+      );
+};
+
+const slidePrev = (index, direction = 1, duration = slide.duration) => {
+  const tl = gsap.timeline({ defaults: { duration: duration, ease: slide.ease } });
+
+  tl.to(
+      slide.elements[index],
+      {
+          yPercent: direction > 0 ? -100 : 100,
+      },
+  ).
+      to(
+          slide.images[index],
+          {
+              yPercent: direction > 0 ? 100 : -100,
+              scale: 0,
+          },
+          0
+      );
+}
+
+
+slide.elements.forEach((el, index) => {
+  index === current ? slideNext(index, 1, 0) : slidePrev(index, 1, 0);
+});
+
+
+
+slide.prev.addEventListener('click', () => {
+  const prev = current;
+  current = slidesWrap(current - 1);
+
+  slideNext(current, -1);
+  slidePrev(prev, -1);
+});
+
+slide.next.addEventListener('click', () => {
+  const prev = current;
+  current = slidesWrap(current + 1);
+
+  slideNext(current);
+  slidePrev(prev);
+});
+
+
+// Showreel
+
+window.onload = function () {
+  let cosmos = document.querySelectorAll(".cosmos");
+  let total = cosmos.length;
+
+  for(let i = 0; i < total; i++) {
+      let angle = (360/total)*i;
+      let transform = `rotate(${angle}deg) translate(55vh)`;
+      cosmos[i].style.transform = transform;
+      let cosmosItems = cosmos[i].querySelectorAll('.cosmic-item');
+
+      for(let j = 0; j < cosmosItems.length; j++) {
+          cosmosItems[j].style.animationDelay = `${j * 0.5}s`
+      }
+  }
+
+
+};
